6ed1213b0bf514660850d51bdd2e003139cec366
faca69 <fnikolov123456789@gmail.com>
2023-12-05 22:49:48 +0100

    Javascript Domasno 1

---
+++ index.js
@@ -0,0 +1,19 @@
+
+
+
+
+let phonePrice = 119.95;
+let numberOfPhones = 30;
+let tax = 5;
+
+let priceWithTax = phonePrice + ((phonePrice * tax) / 100);
+let everyPhoneWithTax = priceWithTax * numberOfPhones;
+
+
+
+console.log(everyPhoneWithTax);
+
+
+
+
+
