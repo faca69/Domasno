5e198fad8954a88b5dfe2407d63ff9f1ced21c0b
faca69 <fnikolov123456789@gmail.com>
2023-12-08 15:31:32 +0100

    Javascript Homework 3

---
+++ Javascript_Homework_3/script.js
@@ -0,0 +1,73 @@
+
+// Dog Calc
+
+// function dogAgeCalcuator (humanYears,) {
+
+//     let dogAgeFormula = humanYears * 7;
+
+//     console.log(dogAgeFormula);
+// }
+
+// dogAgeCalcuator(5);
+
+
+
+
+
+////////////////////////////////////////////////////////////////////
+
+
+
+// typeof
+
+// function checkType(input) {
+
+
+//     let type = typeof input;
+
+//     console.log(`Type of ${input} is: ${type}`);
+// }
+
+// checkType(null)
+// checkType(false)
+// checkType(55)
+// checkType("Hello")
+// checkType()
+
+
+
+
+
+/////////////////////////////////////////////////////////////////
+
+
+
+
+// ATM
+
+// let accountBalance = 1000; 
+
+// function withdrawCash(amount) {
+//     if (amount > accountBalance) {
+//         return "Nemate dovolno pari";
+//     } else {
+//         accountBalance -= amount;
+//         return `Pari izvadeni: ${amount}, Vi ostanuvaat: ${accountBalance}`;
+//     }
+// }
+
+
+// function ATMTransaction() {
+//     let userInput = prompt("Vnesete ja sumata na kolku shto sakate da izvadite:");
+
+//     if (userInput === null || userInput === "") {
+//         alert("Transakcijata e odbiena ili invalid input");
+//     } else {
+//         let amountToWithdraw = parseFloat(userInput);
+//         let result = withdrawCash(amountToWithdraw);
+
+//         alert(result);
+//     }
+// }
+
+// ATMTransaction();
